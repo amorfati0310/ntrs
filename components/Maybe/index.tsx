77fab63a0684dde7e95609b292be5695289b1b74
amorfati0310 <lyhy0310@gmail.com>
2020-09-23 13:48:47 +0900

    articleList 추가

---
+++ components/Maybe/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { ReactChildren } from "react";
+
+interface MaybeProps {
+    test: boolean;
+    children: ReactChildren;
+}
+
+function Maybe({ test, children }: MaybeProps): JSX.Element | null {
+    if (!test) {
+        return null;
+    }
+    return (
+        <>
+            {children}
+        </>
+    );
+}
+
+export default Maybe;
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,10 +1,12 @@
 import { InferGetServerSidePropsType, GetServerSideProps, GetServerSidePropsResult } from 'next';
+import Head from "next/head";
 import styled from '@emotion/styled';
 import { ParsedUrlQuery } from 'querystring';
 
 import Header from 'components/Header';
 import Banner from 'components/Banner';
 import Tags from 'components/Tags';
+import React from 'react';
 
 
 type Author = {
@@ -64,14 +66,23 @@ function HomePage({ data: { tags } }: InferGetServerSidePropsType<typeof getServ
     console.log(tags);
     // Render data...
     return (
-        <div>
-            <Header title="conduit" />
-            <Banner />
-            <MainBlock>
-                <section>Cards</section>
-                <Tags tags={tags} />
-            </MainBlock>
-        </div>
+        <>
+            <Head>
+                <title>HOME | NEXT REALWORLD</title>
+                <meta
+                    name="description"
+                    content="Next.js + SWR codebase containing realworld examples (CRUD, auth, advanced patterns, etc) that adheres to the realworld spec and API"
+                />
+            </Head>
+            <div>
+                <Header title="conduit" />
+                <Banner />
+                <MainBlock>
+                    <section>Cards</section>
+                    <Tags tags={tags} />
+                </MainBlock>
+            </div>
+        </>
     );
 }
 
