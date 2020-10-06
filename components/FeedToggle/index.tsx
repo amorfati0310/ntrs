2d822c3fff60aded2dd913c4f3efdb8b84b1054d
amorfati0310 <lyhy0310@gmail.com>
2020-10-06 23:38:55 +0900

    add serverside emotion dependency and document.tsx

---
+++ components/FeedToggle/index.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+interface FeedToggleProps {
+    text: string;
+}
+
+
+
+export default function FeedToggle({ text }: FeedToggleProps) {
+    return (
+        <div>1</div>
+    );
+}
---
+++ pages/_documents.tsx
@@ -0,0 +1,37 @@
+import Document, { Head, Main, NextScript, DocumentContext } from 'next/document';
+import { extractCritical } from 'emotion-server';
+
+export default class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext): Promise<{
+        styles: JSX.Element;
+        html: string;
+        head?: (JSX.Element | null)[] | undefined;
+    }> {
+        const initialProps = await Document.getInitialProps(ctx);
+        const styles = extractCritical(initialProps.html);
+        return {
+            ...initialProps,
+            styles: (
+                <>
+                    {initialProps.styles}
+                    <style
+                        data-emotion-css={styles.ids.join(' ')}
+                        dangerouslySetInnerHTML={{ __html: styles.css }}
+                    />
+                </>
+            ),
+        };
+    }
+
+    render(): JSX.Element {
+        return (
+            <html lang="ko">
+                <Head />
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </html>
+        );
+    }
+}
